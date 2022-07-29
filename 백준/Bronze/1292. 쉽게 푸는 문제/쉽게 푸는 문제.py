2cae20b80c59f2d3a0518a73215c8f2642dd7060
buzzzlight <pomodoro5292@gmail.com>
2022-07-29 11:19:12 +0900

    dictionary 풀이 추가

--- 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
@@ -5,4 +5,23 @@ for i in range(1, B + 1):
     for j in range(i):
         number.append(i)
 
+# A부터 B까지 슬라이싱
 print(sum(number[A-1:B]))
+
+# 리스트 길이가 1000인 범위 구하기
+# number = []
+
+# for i in range(1, 46):
+#     for j in range(i):
+#         number.append(i)
+
+# print(len(number))
+
+# while 문
+N = 1
+
+while len(number) < B:
+    for i in range(N):
+        number.append(N)
+    N += 1
+print(number)
