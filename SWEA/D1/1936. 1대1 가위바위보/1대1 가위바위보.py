ac21b2844e7838f81e24329c48f005e9d7de0e73
Subin <87738834+buzzzlight@users.noreply.github.com>
2022-08-04 16:47:47 +0900

    [D1] Title: 1대1 가위바위보, Time: 144 ms, Memory: 58,512 KB -BaekjoonHub

---
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.py
@@ -0,0 +1,17 @@
+A, B = input().split()
+
+if A == '1':
+    if B == '2':
+        print('B')
+    else:
+        print('A')
+if A == '2':
+    if B == '1':
+        print('A')
+    else:
+        print('B')
+if A == '3':
+    if B == '1':
+        print('B')
+    else:
+        print('A')
