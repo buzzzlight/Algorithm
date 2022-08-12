7991e3a08d0a7f9c9ce6754a36973312df6006c2
Subin <87738834+buzzzlight@users.noreply.github.com>
2022-08-13 01:10:49 +0900

    [Bronze II] Title: 음계, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,16 +1,8 @@
-list_ = list(map(int, input().split()))
+arr = list(map(int,input().split()))
 
-list_ascending = []
-for i in range(1, 9):
-    list_ascending.append(i)
-
-list_descending = []
-for i in range(8, 0, -1):
-    list_descending.append(i)
-
-if list_ == list_ascending:
+if arr == sorted(arr):
     print('ascending')
-elif list_ == list_descending:
+elif arr == sorted(arr, reverse=True):
     print('descending')
 else:
     print('mixed')
