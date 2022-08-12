1538ce5622c193ca1a972821311391cbebd44661
Subin <87738834+buzzzlight@users.noreply.github.com>
2022-08-13 00:42:59 +0900

    Update 유학 금지.py

--- 백준/Bronze/2789. 유학 금지/유학 금지.py
+++ 백준/Bronze/2789. 유학 금지/유학 금지.py
@@ -5,3 +5,13 @@ for i in word:
     if i in ban:
         word = word.replace(i, '')
 print(word)
+
+# 이게 더 좋을듯 ...
+word = input()                        
+remove_word = list('CAMBRIDGE')       
+new_word = ''                         # 새로운 문자열 받을 곳 만듬
+for i in word:                        
+    if not i in remove_word:          # 겹치는 것이 없는 i를
+        new_word += i                 # new_word에 추가시킴
+
+print(new_word)
